ca570052acf6aadb472700dbb112cf87940802e1
Nataliia Lazorenko <natalystruk@gmail.com>
2021-03-28 21:07:41 +0300

    Added work with localStorage using the library redux-persist

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
-import React, { Component } from 'react';
-// import shortid from 'shortid';
-
+import React from 'react';
 import Container from './components/Container';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
 
-class App extends Component {
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
+const App = () => (
+  <Container>
+    <h1>Phonebook</h1>
+    <ContactForm />
 
-  //   if (parsedContacts) {
-  //     this.setState({ contacts: parsedContacts });
-  //   }
-  // }
-
-  // componentDidUpdate(prevState) {
-  //   const { contacts } = this.state;
-
-  //   if (contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(contacts));
-  //   }
-  // }
-
-  render() {
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm />
-
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </Container>
-    );
-  }
-}
+    <h2>Contacts</h2>
+    <Filter />
+    <ContactList />
+  </Container>
+);
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,21 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import contactsReducer from './contacts/contacts-reducer';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
 const rootReducer = combineReducers({
-  contacts: contactsReducer,
+  contacts: persistedReducer,
 });
 
 const store = createStore(rootReducer, composeWithDevTools());
+const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
